515828dbb0097f948357b8fdc84e00193e7a33a8
Alexey Kalinin <lyoffa412@gmail.com>
2019-09-23 11:34:08 +0300

    setup.py

---
+++ setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="itlogist-client",
+    version="0.0.1",
+    author="alkalit",
+    author_email="lyoffa412@gmail.com",
+    description="Простой клиент для API itlogist",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
