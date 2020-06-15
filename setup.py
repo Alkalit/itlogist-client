e859a8ba02e810db64ee777b4efc4960d122b554
Alexey Kalinin <lyoffa412@gmail.com>
2020-06-15 16:56:06 +0300

    bump version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="itlogist-client",
-    version="0.0.1",
+    version="0.0.2",
     author="alkalit",
     author_email="lyoffa412@gmail.com",
     description="Простой клиент для API itlogist",
